4e8263fc26a10e13d06e3e6234405f468a665e31
AmirHosein-Vahed <vahed.ahv@gmail.com>
2025-04-16 16:46:47 +0330

    handle response

--- app/main.py
+++ app/main.py
@@ -8,17 +8,14 @@ def main():
     # Uncomment this to pass the first stage
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
 
     while True:
-        conn, addr = server_socket.accept()
-        print(f"connection from {addr}")
-
-        response = "HTTP/1.1 200 OK\r\n\r\n"
-        
-        conn.sendall(response.encode())
-
+        conn, addr = server_socket.accept() # wait for client
+        print(f"Request from {addr}")
+        conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
         conn.close()
+            
+    
 
 
 if __name__ == "__main__":
