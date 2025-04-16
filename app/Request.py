dc3cf91b7143bd28a5363c2d4b0365af6e650541
AmirHosein-Vahed <vahed.ahv@gmail.com>
2025-04-16 20:31:38 +0330

    separate user-agent data from other property of class Request

--- app/Request.py
+++ app/Request.py
@@ -4,7 +4,10 @@ class Request:
         self.address    = address
         self.data       = self.connection.recv(1024).decode().split("\r\n")
         self.URLpath    = self.data[0].split(" ")[1]
-        self.user_agent = self.data[2].split(":")[1].lstrip()
+        # self.user_agent = self.data[2].split(":")[1].lstrip()
+
+    def set_user_agent(self):
+        self.user_agent = self.data[2].split(" ")[1]
         
     def print_data(self):
         print("User-Agent: ", self.user_agent)
