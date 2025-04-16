7c22724022198bb73392c4264c1d5e39fb73ec8a
AmirHosein-Vahed <vahed.ahv@gmail.com>
2025-04-16 20:25:47 +0330

    separating with : instead of space

--- app/Request.py
+++ app/Request.py
@@ -4,7 +4,7 @@ class Request:
         self.address    = address
         self.data       = self.connection.recv(1024).decode().split("\r\n")
         self.URLpath    = self.data[0].split(" ")[1]
-        self.user_agent = self.data[2].split(" ")[1]
+        self.user_agent = self.data[2].split(":")[1]
         
     def print_data(self):
-        print("data: ", self.data)
+        print("User-Agent: ", self.user_agent)
